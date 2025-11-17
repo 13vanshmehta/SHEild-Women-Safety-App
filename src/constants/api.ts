17286995088ffb8e3bb20eb53def668c70de529d
Vansh Mehta <mehtavansh13042005@gmail.com>
2025-11-17 10:09:47 +0530

    env changess

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
+import Config from 'react-native-config';
+
 // API Configuration
 export const API_CONFIG = {
   BASE_URL: 'http://192.168.29.17:8000', // Update this with your actual IP
@@ -20,7 +22,10 @@ export const APP_CONFIG = {
   APP_NAME: 'SHEild',
   APP_SUBTITLE: 'Women Safety App',
   VERSION: '1.0.0',
-  ONBOARDING_TIMEOUT: 2000, // 2 seconds
+  ONBOARDING_TIMEOUT: 4000, // 4 seconds
 };
 
-export const GEOAPIFY_API_KEY = 'e2999a4e883c4232b894f21b21bf7a49';
+// export const GEOAPIFY_API_KEY = 'e2999a4e883c4232b894f21b21bf7a49';
+
+// Use environment variable or fallback to default
+export const GEOAPIFY_API_KEY = Config.GEOAPIFY_API_KEY || 'e2999a4e883c4232b894f21b21bf7a49'
